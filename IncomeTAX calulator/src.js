b58f0d7f5eb541f8cf63ffbd76df2c462b874185
arbaaz19t <mdarbaaz4419@gmail.com>
2026-03-03 00:47:34 +0530

    IncomeTaxCalci

--- IncomeTAX calulator/src.js
+++ IncomeTAX calulator/src.js
@@ -5,17 +5,16 @@ form.addEventListener('submit', function (e) {
     const AmountEarn = parseInt(Income.value);
     const REsult = document.querySelector('h2');
     let TotalTAx = 0;
-    if (AmountEarn <= 1200000){
-        TotalTAx = 0;}
+    if (AmountEarn <= 1200000) {
+        TotalTAx = 0;
+    } else if (AmountEarn <= 1600000) {
+        TotalTAx = (AmountEarn - 1200000) * 0.15;
 
-    else if (AmountEarn <= 1600000){
-        TotalTAx = (AmountEarn - 1200000) * 0.15;}
+    }else if (AmountEarn <= 2000000) {
+        TotalTAx = (AmountEarn - 1200000) * 0.20 + 60000;
 
-    else if (AmountEarn <= 2000000){
-        TotalTAx = (AmountEarn - 1200000) * 0.20 + 60000;}
-
-    else (AmountEarn <= 2400000)
-    TotalTAx = (AmountEarn - 1200000) * 0.25 + 60000 + 80000;
+    } else if (AmountEarn <= 2400000)
+        TotalTAx = (AmountEarn - 1200000) * 0.25 + 60000 + 80000;
 
     REsult.textContent = `TOTAL TAX : ${TotalTAx}`;
     form.reset()
